fa4c288a45fbc787c1432073bbdd4489bfba7641
Subhash <subhashbantu88@gmail.com>
2026-05-18 14:19:07 +0530

    Added Day 2 practice

--- day2.py
+++ day2.py
@@ -10,7 +10,7 @@ def add_numbers():
 
     print(first_number+second_number)
 
-add_numbers()"""
+add_numbers()
 
 number = int(input("enter a number:"))
 def is_even(number):
@@ -23,3 +23,19 @@ if is_even(number):
     print("it is even")
 else:
     print("its odd")
+
+
+
+user = input("enter your name:")
+def greet_name(user):
+    print(f"welcome {user}")
+          
+greet_name(user)"""
+
+
+
+number =int(input("enter a value:"))
+def is_square(numb):
+    return numb ** 2
+result = is_square(number)
+print(result)
