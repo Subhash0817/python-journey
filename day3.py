22de29ee40b5795666162a25234537763a85c8d5
Subhash <subhashbantu88@gmail.com>
2026-05-20 14:45:47 +0530

    Added Day 3 Python concepts and list practice

---
+++ day3.py
@@ -0,0 +1,21 @@
+def greet(name):
+    print(f"hello {name}")
+
+greet("subhash")
+
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)
+
+
+games = []
+
+games.append("valorant")
+games.append("minecraft")
+games.append("gta 5")
+
+for game in games:
+    print(game)
