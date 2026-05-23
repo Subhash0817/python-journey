703f88ff317da97d64feb634d83cd299ae925652
Subhash <subhashbantu88@gmail.com>
2026-05-23 14:06:04 +0530

    Completed Day 6 dictionaries and DSA practice

--- day6.py
+++ day6.py
@@ -19,3 +19,25 @@ student["age"] = input("enter student age:")
 student["course"] = input("enter student course:")
 
 print(student)
+
+
+
+player = {
+    "username" : "subhash",
+    "level" : "21",
+    "score" : "30",
+    "weapon": "operator"
+}
+player ["level"] = 90
+print(f"Welcome {player['username']}")
+print(f"Player level is {player['level']}")
+print(f"Player weapon is {player['weapon']}")
+
+
+player = {}
+player["name"] = input("enter user name:")
+player["level"] = input("enter user level:")
+player["score"] = input("enter user score:")
+player["weapon"] = input("enter user weapon:")
+
+print(player)
