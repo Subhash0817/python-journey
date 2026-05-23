75b19810adaa777ffeb7c6b07863cf2397a91138
Subhash <subhashbantu88@gmail.com>
2026-05-23 13:50:14 +0530

    Completed Day 6 dictionaries and DSA practice

---
+++ day6.py
@@ -0,0 +1,21 @@
+student = {
+    "name" : "subhash",
+    "age" : "21",
+    "course": "cse"
+}
+print(student ["name"])
+print(student ["age"])
+student["age"] = 22
+
+
+student["skills"] = "Python"
+
+print(student)
+
+
+student ={}
+student["name"] = input("enter student name:")
+student["age"] = input("enter student age:")
+student["course"] = input("enter student course:")
+
+print(student)
