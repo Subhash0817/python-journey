49b24bcda641d73fb2f9baaa1e6d46365f5affa3
Subhash <subhashbantu88@gmail.com>
2026-05-24 17:37:07 +0530

    Solved and updated even and odd DSA problem

--- dsa/findevenodd.py
+++ dsa/findevenodd.py
@@ -9,3 +9,15 @@ for number in numbers:
     if number % 2 != 0:
         odd_numbers.append(number)
 print(odd_numbers)
+
+numbers = [4, 7, 2, 9, 10, 13, 8]
+even_numbers = []
+odd_numbers =[]
+
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        odd_numbers.append(number)
+print(even_numbers)
+print(odd_numbers)
