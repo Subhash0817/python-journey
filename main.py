ed0ea0b6bc0eb55105c2c912b7b873c04f5e4b2a
Subhash <subhashbantu88@gmail.com>
2026-05-26 15:20:14 +0530

    Built random number guessing game using modules

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ from maths_utils import add, sub, mul
 print(add(5, 3))
 print(sub(10, 2))
 print(mul(4, 6))
+
+
+import maths_utils
+print(maths_utils.add(5, 3))
+print(maths_utils.sub(10, 2))
+
+
+import random
+number = random.randint(1,10)
+attempts = 0
+while True:
+    guess = int(input("guess a number: "))
+    attempts += 1
+    if guess == number:
+        print("correct")
+        print(f"You guessed in {attempts} attempts")
+    elif guess > number:
+        print("too high")
+       
+    else:
+        print("too low")
