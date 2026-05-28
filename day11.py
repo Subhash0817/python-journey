8fe6676212b5c1faf77b4d3b3734b0f8bbbb3c59
Subhash <subhashbantu88@gmail.com>
2026-05-28 17:15:24 +0530

    Learned list comprehensions and data filtering

---
+++ day11.py
@@ -0,0 +1,41 @@
+"""list comprehension"""
+numbers = [1,2,3,4,5,6,7,8,9,10]
+even_numbers = [number for number in numbers if number %2 == 0]
+print(even_numbers)
+odd_numbers = [number for number in numbers if number %2 != 0]
+print(odd_numbers)
+
+names = ["subhash", "v", "sage"]
+upper_names = [name.upper() for name in names]
+print(upper_names)
+
+
+names = ["subhash", "v", "sage", "sova", "jett"]
+
+filtered_names = [name for name in names if name.startswith("s")]
+
+print(filtered_names)
+
+
+weapons = [
+    {"name": "vandal", "damage": 160},
+    {"name": "ghost", "damage": 80},
+    {"name": "operator", "damage": 250}
+]
+filtered_weapons =[
+    weapon for weapon in weapons 
+    if weapon ["damage"] > 100
+]
+print(filtered_weapons)
+
+players = [
+    {"name": "subhash", "score": 90},
+    {"name": "v", "score": 40},
+    {"name": "sage", "score": 75}
+]
+
+filtered_players = [
+    player for player in players
+    if player ["score"] >= 70
+]
+print(filtered_players)
