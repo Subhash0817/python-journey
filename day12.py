66f15d6fd2fd4ed9c347e8aa6caf8eecfb6f096c
Subhash <subhashbantu88@gmail.com>
2026-05-29 15:04:31 +0530

    Learned OOP basics with player class methods

--- day12.py
+++ day12.py
@@ -12,14 +12,33 @@ print(player1.level, player2.level)
 
 class Player:
 
-    def __init__(self, name, level):
+    def __init__(self, name, level, health, damage):
         self.name = name
         self.level = level
-
+        self.health = health
+        self.damage = damage
+    def take_damage(self, amount):
+        self.health -= amount
+        print(self.name, "took", amount, "damage")
+    def heal(self, amount):
+        self.health += amount
+        print(self.name, "healed", amount, "health")
+    def level_up(self):
+            self.level += 1
+            print(self.name, "leveled up!")
     def show_info(self):
         print("Name:", self.name)
         print("Level:", self.level)
-player1 = Player("Subhash", 21)
-player2 = Player("V", 25)
+        print("health:", self.health)
+        print("damage:", self.damage)
+        
+
+        
+player1 = Player("Subhash", 21, 100, 160)
+
 player1.show_info()
-player2.show_info()
+player1.take_damage(20)
+player1.heal(10)
+player1.level_up()
+
+player1.show_info()
